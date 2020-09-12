b2b7808879419466d5660a23d437ce47869f3836
CrisSherban <cris.sherban.t@gmail.com>
2020-09-12 18:32:33 +0200

    Added split_data()

--- check_model.py
+++ check_model.py
@@ -2,16 +2,11 @@ from tensorflow import keras
 from training import load_data
 import numpy as np
 
-print("creating untouched training_data")
-untouched = load_data(starting_dir="untouched_data")
-untouched_X = []
-untouched_y = []
-for X, y in untouched:
-    untouched_X.append(X)
-    untouched_y.append(y)
+print("loading untouched_data")
+untouched_X, untouched_y = load_data(starting_dir="untouched_data")
 
 untouched_X = np.array(untouched_X)[:, :, :, np.newaxis]
 untouched_y = np.array(untouched_y)
 
-model = keras.models.load_model('models/best.model')
+model = keras.models.load_model('models/best2.model')
 model.evaluate(untouched_X, untouched_y)
--- training.py
+++ training.py
@@ -20,26 +20,85 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 ACTIONS = ["left", "right", "none"]
 
 
-def normalize(data):
-    # for some reason normalizing this dataset gives far worse accuracies
-    samples = len(data)
-    channels = len(data[0])
-    frequency = len(data[0, 0])
+def normalize(data, individual_samples=True):
+    if individual_samples:
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        for sample in range(len(data)):
+            data[sample] = scaler.fit_transform(data[sample])
 
-    data = np.reshape(data, (samples * channels, frequency))
+    else:
+        # for some reason normalizing this dataset gives far worse accuracies
+        samples = len(data)
+        channels = len(data[0])
+        frequency = len(data[0, 0])
 
-    plt.plot(np.arange(len(data[0])), data[0])
-    plt.show()
+        data = np.reshape(data, (samples * channels, frequency))
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    data = scaler.fit_transform(data)
+        plt.plot(np.arange(len(data[0])), data[0])
+        plt.show()
 
-    plt.plot(np.arange(len(data[0])), data[0])
-    plt.show()
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        data = scaler.fit_transform(data)
 
-    data = np.reshape(data, (samples, channels, frequency))
+        plt.plot(np.arange(len(data[0])), data[0])
+        plt.show()
+
+        data = np.reshape(data, (samples, channels, frequency))
 
     return data
+
+
+def split_data(starting_dir="data", splitting_percentage=(65, 20, 15)):
+    """
+        This function splits the dataset in three folders, training, validation, untouched
+        Has to be run just everytime the dataset is changed
+
+    :param starting_dir: string, the directory of the dataset
+    :param splitting_percentage:  tuple, (training_percentage, validation_percentage, untouched_percentage)
+    """
+    training_per, validation_per, untouched_per = splitting_percentage
+
+    # creating directories
+    tmp_dir = os.path.join("training_data")
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    tmp_dir = os.path.join("validation_data")
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    tmp_dir = os.path.join("untouched_data")
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+
+    data = {}
+    for action in ACTIONS:
+        if action not in data:
+            data[action] = []
+
+        data_dir = os.path.join(starting_dir, action)
+        for file in os.listdir(data_dir):
+            # each item is a ndarray of shape (8, 90) that represents ~= 1sec of acquisition
+            data[action].append(np.load(os.path.join(data_dir, file)))
+
+        np.random.shuffle(data[action])
+
+        # creating subdirectories for each action
+        tmp_dir = os.path.join("training_data", action)
+        if not os.path.exists(tmp_dir):
+            os.mkdir(tmp_dir)
+        for sample in range(int(len(data[action]) * training_per / 100)):
+            np.save(file=os.path.join(tmp_dir, str(sample)), arr=data[action][sample])
+
+        tmp_dir = os.path.join("validation_data", action)
+        if not os.path.exists(tmp_dir):
+            os.mkdir(tmp_dir)
+        for sample in range(int(len(data[action]) * validation_per / 100)):
+            np.save(file=os.path.join(tmp_dir, str(sample)), arr=data[action][sample])
+
+        tmp_dir = os.path.join("untouched_data", action)
+        if not os.path.exists(tmp_dir):
+            os.mkdir(tmp_dir)
+        for sample in range(int(len(data[action]) * untouched_per / 100)):
+            np.save(file=os.path.join(tmp_dir, str(sample)), arr=data[action][sample])
 
 
 def load_data(starting_dir="training_data"):
@@ -59,13 +118,12 @@ def load_data(starting_dir="training_data"):
         data_dir = os.path.join(starting_dir, action)
         for file in os.listdir(data_dir):
             # each item is a ndarray of shape (8, 90) that represents ~= 1sec of acquisition
-            data = np.load(os.path.join(data_dir, file))
-            data[action].append(data)
+            data[action].append(np.load(os.path.join(data_dir, file)))
 
     lengths = [len(data[action]) for action in ACTIONS]
     print(lengths)
 
-    # this is required if there are more samples in a class
+    # this is required if the classes are unbalanced
     for action in ACTIONS:
         np.random.shuffle(data[action])
         data[action] = data[action][:min(lengths)]
@@ -74,31 +132,27 @@ def load_data(starting_dir="training_data"):
     print(lengths)
 
     # creating X, y
-    combined_data = []  # this is necessary to shuffle the training_data
+    X = []
+    y = []
 
     for action in ACTIONS:
         for sample in data[action]:
             if action == "left":
-                combined_data.append([sample, [1, 0, 0]])
+                X.append(sample)
+                y.append([1, 0, 0])
             elif action == "right":
-                combined_data.append([sample, [0, 0, 1]])
+                X.append(sample)
+                y.append([0, 0, 1])
             elif action == "none":
-                combined_data.append([sample, [0, 1, 0]])
-
-    np.random.shuffle(combined_data)
-    print("length:", len(combined_data))
-
-    X = []
-    y = []
-
-    for sample, label in data:
-        X.append(sample)
-        y.append(label)
+                X.append(sample)
+                y.append([0, 1, 0])
 
     return X, y
 
 
 def main():
+    # split_data()
+
     print("loading training_data")
     train_X, train_y = load_data(starting_dir="training_data")
 
@@ -123,10 +177,21 @@ def main():
 
     print(np.array(train_X).shape)
 
-    for i in range(8):
-        plt.plot(np.arange(len(train_X[0][i])), train_X[0][i].reshape((90)))
-    plt.show()
+    # taking a look at the data so far
+    fig, ax = plt.subplots(3)
+    fig.suptitle('Train, Validation, Untouched')
+    for j in range(4):
+        for i in range(8):
+            ax[0].plot(np.arange(len(train_X[j][i])), train_X[j][i].reshape((90)))
+        for i in range(8):
+            ax[1].plot(np.arange(len(validation_X[j][i])), validation_X[j][i].reshape((90)))
+        for i in range(8):
+            ax[2].plot(np.arange(len(untouched_X[j][i])), untouched_X[j][i].reshape((90)))
+        plt.savefig(str(j) + ".png")
+        for i in range(3):
+            ax[i].clear()
 
+    '''
     ####### an toy model #######
     inputs = tf.keras.Input(shape=(8, 90, 1), name="fft")
     x = Conv2D(filters=32, kernel_size=(3, 3), activation='tanh', padding="same")(inputs)
@@ -144,11 +209,12 @@ def main():
     outputs = Dense(3, activation="softmax")(x)
 
     model = tf.keras.Model(inputs, outputs, name="crisnet")
+    '''
 
     #########################################################
-    '''  ### a simpler model ###
+    #################### a simpler model ####################
     model = Sequential([
-        Conv2D(filters=32, kernel_size=(3, 3), activation='tanh', 
+        Conv2D(filters=32, kernel_size=(3, 3), activation='tanh',
                padding="same", input_shape=(8, 90, 1)),
 
         MaxPool2D(pool_size=(2, 2), strides=2),
@@ -163,7 +229,7 @@ def main():
 
         Dense(3, activation="softmax")
     ])
-    '''
+
     ##########################################################
 
     model.summary()
